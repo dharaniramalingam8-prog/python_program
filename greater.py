604d4d0439a537e2534e2f692e5b11bad83c3059
dharani ramalingam <dharaniramalingam8@@gmail.com>
2026-01-03 15:15:08 +0530

    added

---
+++ greater.py
@@ -0,0 +1,9 @@
+# Take input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Find and display the larger number
+if num1 > num2:
+    print(f"{num1} is larger")
+else:
+    print(f"{num2} is larger")
